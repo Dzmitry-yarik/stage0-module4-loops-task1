bc94daf0509837ed750f8ad715563fefb359df36
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-09 20:14:52 +0300

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,11 +1,10 @@
 package school.mjc.stage0.loops.task1;
-
+import java.util.Collections;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-        String q = "writing the same code doesn't have much impact, and it's also time-consuming\n";
-        System.out.print(q.repeat(20));
+        System.out.print(String.join("", Collections.nCopies(20, "writing the same code doesn't have much impact, and it's also time consuming\n")));
 //        System.out.println();
 //        System.out.println();
 //        System.out.println();
